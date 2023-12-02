914a2411d25cec875b2501c3837caf27a7efe0d7
Michal Viwegier <beebas@wp.pl>
2023-12-03 00:10:23 +0100

    1-4 begin

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -8,8 +8,6 @@ const element = React.createElement(
 );
 
 const elementInJSX = <a href="https://zrozumiecreact.pl">Test 3</a>;
-console.log(<h1>Test 2</h1>); // -> React.createElement('h1', {}, 'Test 2')
-console.log(element);
 
 const currentDate = new Date();
 
