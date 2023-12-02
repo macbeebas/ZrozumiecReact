16d7566f7874f759338798593176c061ffad7bff
Michal Viwegier <beebas@wp.pl>
2023-12-02 23:38:24 +0100

    3 step

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -8,8 +8,20 @@ const element = React.createElement(
 );
 
 const elementInJSX = <a href="https://zrozumiecreact.pl">Test 3</a>;
-
 console.log(<h1>Test 2</h1>); // -> React.createElement('h1', {}, 'Test 2')
 console.log(element);
 
-ReactDOM.createRoot(document.getElementById("root")).render(element);
+const currentDate = new Date();
+
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+
+console.log(hours, minutes);
+
+const timer = (
+  <div>
+    {hours}:{minutes}
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(timer);
