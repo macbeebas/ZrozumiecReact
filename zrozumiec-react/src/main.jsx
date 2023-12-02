c1473d8e6000560f87c46d6eff9055de5e0feeb7
Michal Viwegier <beebas@wp.pl>
2023-12-03 00:54:10 +0100

    1-5 step 1

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -1,39 +1,29 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-const stringContent = "Test 1!";
-const numberContent = 13;
-const calcContent = 10 + 5;
-function getRandomNumber() {
-  // return Math.round(Math.random() * 100);
-  console.log(Math.round(Math.random() * 100));
-}
-const person = {
-  name: "Filip",
-  age: 29,
-};
-const paragraphElement = (
-  <>
-    <h2>Heading</h2>
-    <a href="http://zrozumiecreact.pl" className="link" for="">
-      Strona kursu
-    </a>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-      aspernatur cupiditate nulla. Dolorem assumenda, quidem sint ipsum labore
-      id numquam odio, tempora autem provident suscipit ipsam ea sequi illum
-      sapiente.
-    </p>
-  </>
-);
 const element = (
   <div>
-    {paragraphElement}
-    {paragraphElement}
-    {paragraphElement}
-    {paragraphElement}
-    {paragraphElement}
-    {paragraphElement}
+    <h1>Nasza strona!</h1>
+    <main>
+      <a
+        href="http://zrozumiecreact.pl"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Strona kursu
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, saepe
+        cum doloremque fugit id ad ea sed harum vel. Nam odio quos tenetur aut
+        aperiam dolores hic. Dolores, vel eum!
+      </p>
+      <a
+        href="http://zrozumiecreact.pl"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Strona kursu
+      </a>
+    </main>
   </div>
 );
 
