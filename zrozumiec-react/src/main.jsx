97ec487e2c45b8328712aeb1a03c1b69998dc6c6
Michal Viwegier <beebas@wp.pl>
2023-12-03 00:20:53 +0100

    1-4 step 1

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = <div>TEST</div>;
+const stringContent = "Test 1!";
+const numberContent = 13;
+const calcContent = 10 + 5;
+const element = (
+  <div>
+    {stringContent}, {numberContent}, {calcContent}, {20 - 5 - 3},{" "}
+    {"Test string"}, {`Number: ${numberContent}`}
+  </div>
+);
+const address = "https://zrozumiecreact.pl";
+const linkElement = <a href={address}>{address}</a>;
 
-ReactDOM.createRoot(document.getElementById("root")).render(element);
+ReactDOM.createRoot(document.getElementById("root")).render(linkElement);
