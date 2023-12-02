8d9632212e684af3a15a7754d451cfbbf3710b46
Michal Viwegier <beebas@wp.pl>
2023-12-03 00:13:35 +0100

    1-4 begin step 0

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -1,25 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = React.createElement(
-  "a",
-  { href: "https://zrozumiecreact.pl" },
-  "Test"
-);
+const element = <div>TEST</div>;
 
-const elementInJSX = <a href="https://zrozumiecreact.pl">Test 3</a>;
-
-const currentDate = new Date();
-
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-
-console.log(hours, minutes);
-
-const timer = (
-  <div>
-    {hours}:{minutes}
-  </div>
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(timer);
+ReactDOM.createRoot(document.getElementById("root")).render(element);
