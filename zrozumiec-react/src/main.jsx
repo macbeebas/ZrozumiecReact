53a4300792588aad35a5288f0daab2af112728c2
Michal Viwegier <beebas@wp.pl>
2023-12-02 23:23:04 +0100

    2 step

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -7,4 +7,9 @@ const element = React.createElement(
   "Test"
 );
 
+const elementInJSX = <a href="https://zrozumiecreact.pl">Test 3</a>;
+
+console.log(<h1>Test 2</h1>); // -> React.createElement('h1', {}, 'Test 2')
+console.log(element);
+
 ReactDOM.createRoot(document.getElementById("root")).render(element);
