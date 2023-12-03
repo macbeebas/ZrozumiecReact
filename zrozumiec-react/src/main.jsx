d282103c0af1e8756eb602e28a87e12262a7168e
Michal Viwegier <beebas@wp.pl>
2023-12-03 22:09:32 +0100

    1-6 step 1

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -5,13 +5,14 @@ const element = (
   <>
     <h1>Nasza strona!</h1>
     <main>
-      <Link />
+      <Link text="Sprawdź strone kursu!" />
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere labore
         blanditiis magnam eveniet rem ratione, voluptatum nulla inventore
         quaerat necessitatibus.
       </p>
-      <Link />
+      <Link text="https://zrozumiecreact.pl" />
+      {/* -> jak funkcja 'Link({text: "https://zrozumiecreact.pl"})', która otrzymuje jako propsy obiekt */}
     </main>
   </>
 );
