c5b526933a5d1f1d46ffb5f3096e261d4dfc511d
Michal Viwegier <beebas@wp.pl>
2023-12-03 01:05:47 +0100

    1-5 step 3

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -2,15 +2,28 @@ import ReactDOM from "react-dom/client";
 
 const linkElement = (
   <a href="http://zrozumiecreact.pl" target="_blank" rel="noopener noreferrer">
-    Strona kursu
+    Sprawdź już teraz!
   </a>
 );
+
+function LinkComponent() {
+  return (
+    <a
+      href="http://zrozumiecreact.pl"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Sprawdź już teraz!
+    </a>
+  );
+}
 
 const element = (
   <>
     <h1>Nasza strona!</h1>
     <main>
-      {linkElement}
+      {/* {LinkComponent()} */}
+      <LinkComponent></LinkComponent>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, saepe
         cum doloremque fugit id ad ea sed harum vel. Nam odio quos tenetur aut
