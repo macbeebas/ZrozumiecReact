0caf56cc2fd0e7ca9ec6965c57ef3e7199dab115
Michal Viwegier <beebas@wp.pl>
2023-12-03 23:35:40 +0100

    1-6 step 3

--- zrozumiec-react/src/components/Link.jsx
+++ zrozumiec-react/src/components/Link.jsx
@@ -1,11 +1,16 @@
-export function Link(props) {
-  console.log(props);
+// export function Link(props) {
+// const { text, shouldOpenNewTab } = props;
+//   To to samo co
+//   const text = props.text;
+//   const shouldOpenNewTab = props.shouldOpenNewTab;
+// powyżej wersja z tłumaczenia a poniżej docelowa
+export function Link({ text, shouldOpenNewTab }) {
   const address = "https://zrozumiecreact.pl";
-  const target = props.shouldOpenNewTab ? "_blank" : "";
-  const rel = props.shouldOpenNewTab ? "noopener noreferrer" : "";
+  const target = shouldOpenNewTab ? "_blank" : "";
+  const rel = shouldOpenNewTab ? "noopener noreferrer" : "";
   return (
     <a href={address} target={target} rel={rel}>
-      {props.text}
+      {text}
     </a>
   );
 }
