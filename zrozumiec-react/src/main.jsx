1debb09ab91e335fac4649d2aaead43f107b9d97
Michal Viwegier <beebas@wp.pl>
2023-12-04 20:13:29 +0100

    1-7 end

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -1,9 +1,4 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
