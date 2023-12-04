19eca7792c0149ed899e5ef69d1f6fb3dce81a72
Michal Viwegier <beebas@wp.pl>
2023-12-04 20:03:54 +0100

    1-7 step 4

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -1,14 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { Link } from "./components/Link";
 import { HeadingItalic } from "./components/HeadingItalic";
+import { Logger } from "./components/Logger";
 
 const element = (
   <>
-    <HeadingItalic>
-      <Link />
-    </HeadingItalic>
+    <Logger>
+      <HeadingItalic>Nasza super strona</HeadingItalic>
+    </Logger>
     <main>
-      <Link text="Sprawdź strone kursu!" shouldOpenNewTab={true} />
+      <Logger>
+        <Link text="Sprawdź strone kursu!" shouldOpenNewTab={true} />
+      </Logger>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere labore
         blanditiis magnam eveniet rem ratione, voluptatum nulla inventore
