164cd7a73feb8ccd115ee20c5985d8df1d974ada
Michal Viwegier <beebas@wp.pl>
2023-12-05 23:17:07 +0100

    1-8 step 1

--- zrozumiec-react/src/App.jsx
+++ zrozumiec-react/src/App.jsx
@@ -1,5 +1,26 @@
 function App() {
-  return <div>Test</div>;
+  const jan = {
+    name: "Jan",
+    lastName: "Kowalski",
+    mail: "j.kow@gmail.com",
+    tel: "+48 888 000 111",
+  };
+
+  return (
+    <address>
+      <div>
+        <span>
+          {jan.name} {jan.lastName}
+        </span>
+      </div>
+      <div>
+        Email: <a href={`mailto:${jan.mail}`}>{jan.mail}</a>
+      </div>
+      <div>
+        Telefon: <a href={`tel: ${jan.mail}`}>{jan.mail}</a>
+      </div>
+    </address>
+  );
 }
 
 export default App;
