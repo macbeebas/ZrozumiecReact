94241a5f82ad0cc62914a2bddae45223a4c231b1
Michal Viwegier <beebas@wp.pl>
2023-12-07 19:38:40 +0100

    2-1 step 1

--- zrozumiec-react-2/src/App.jsx
+++ zrozumiec-react-2/src/App.jsx
@@ -1,5 +1,18 @@
 function App() {
-  return <></>;
+  document.querySelector("button").addEventListener("click", () => {
+    alert("Klik");
+  });
+
+  return (
+    <>
+      <h1>Gwiezdne Wojny V</h1>
+      <h2>Rok produkcji: 1980</h2>
+      <h3>Fabuła</h3>
+      <p>Dobrzy walczą ze złymi. Trzeba wyłączyc pole siłowe.</p>
+      <button>Pokaż spoiler</button>
+      <p>Vader okazuje się być ojcem Luka.</p>
+    </>
+  );
 }
 
 export default App;
