3d2d54c73e0803ee457fecf1ab1ee2bde4fcf308
Michal Viwegier <beebas@wp.pl>
2023-12-13 20:32:13 +0100

    3-1

--- zrozumiec-react/src/App.jsx
+++ zrozumiec-react/src/App.jsx
@@ -1,37 +1,5 @@
-import { PersonInfo } from "./components/PersonInfo";
-import imageFromAssets from "./assets/dog-assets.jpg";
-
 function App() {
-  const jan = {
-    name: "Jan",
-    lastName: "Kowalski",
-    mail: "j.kow@gmail.com",
-    tel: "+48 888 000 111",
-  };
-  const janina = {
-    name: "Janina",
-    lastName: "Kowalska",
-    mail: "janina.kow@gmail.com",
-    tel: "+48 888 000 222",
-  };
-  const karol = {
-    name: "Karol",
-    lastName: "Kowalski",
-    mail: "karol.kow@gmail.com",
-  };
-
-  const imageLink = "https://zrozumiecreact.pl/dog.jpg";
-  return (
-    <>
-      <PersonInfo person={jan} />
-      <br />
-      <PersonInfo person={janina} />
-      <br />
-      <PersonInfo person={karol} />
-      <br />
-      <img src={imageLink} alt="" />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
