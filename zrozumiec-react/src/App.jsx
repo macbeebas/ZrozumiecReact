6d8b6f0d57603235a09abf2c72dbe7082bdf0fac
Michal Viwegier <beebas@wp.pl>
2023-12-14 00:07:03 +0100

    3-3g step 7 - 'useState' final version

--- zrozumiec-react/src/App.jsx
+++ zrozumiec-react/src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 
 function App() {
   const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+  const [isWarningShown, setIsWarningShown] = useState(true);
 
-  function handleClick() {
+  function handleShowSpoilerClick() {
     setIsSpoilerShown(true);
     console.log("handleClick!");
+    setIsWarningShown(false);
+  }
+
+  function handleCloseWarningClick() {
+    setIsWarningShown(false);
   }
 
   console.log("<App> - render");
@@ -15,12 +21,18 @@ function App() {
       <h1>Gwiezdne wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
       <h2>Fabuła</h2>
+      {isWarningShown && (
+        <p>
+          Uwaga! Opis fabuły zawiera spoilery!
+          <button onClick={handleCloseWarningClick}>X</button>
+        </p>
+      )}
       <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
 
       {isSpoilerShown ? (
         <p>Vader okazuje się być ojcem Luka.</p>
       ) : (
-        <button onClick={handleClick}>Pokaż spoiler</button>
+        <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button>
       )}
     </>
   );
