754ff7a71beea67c9abea6d0a674fecb9adcc412
Michal Viwegier <beebas@wp.pl>
2023-12-13 21:13:38 +0100

    3-1 step 2 - end code

--- zrozumiec-react/src/App.jsx
+++ zrozumiec-react/src/App.jsx
@@ -1,11 +1,17 @@
 function App() {
+  const buttonEl = <button>Pokaż spoiler</button>;
+
+  // document.querySelector("button").addEventListener("click", () => {
+  //     alert("Klik");
+  // });
+
   return (
     <>
       <h1>Gwiezdne wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
       <h2>Fabuła</h2>
       <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
-      <button>Pokaż spoiler</button>
+      {buttonEl}
       <p>Vader okazuje się być ojcem Luka.</p>
     </>
   );
