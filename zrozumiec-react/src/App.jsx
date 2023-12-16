2af699e2f2b7fcec1e4f1170de8fbed6729dbeff
Michal Viwegier <beebas@wp.pl>
2023-12-17 00:06:58 +0100

    3-6a step 1 - change 3 useStates to one

--- zrozumiec-react/src/App.jsx
+++ zrozumiec-react/src/App.jsx
@@ -1,26 +1,42 @@
 import { useState } from "react";
 
+const initialState = {
+  isSpoilerShown: false,
+  isWarningShown: true,
+  numberOfLikes: 50,
+};
+
 function App() {
-  const [isSpoilerShown, setIsSpoilerShown] = useState(false);
-  const [isWarningShown, setIsWarningShown] = useState(true);
-  const [numberOfLikes, setNumberOfLikes] = useState(50);
+  // const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+  // const [isWarningShown, setIsWarningShown] = useState(true);
+  // const [numberOfLikes, setNumberOfLikes] = useState(50);
+
+  const [state, setState] = useState(initialState);
 
   function handleShowSpoilerClick() {
-    setIsSpoilerShown(true);
-    console.log("handleClick!");
-    setIsWarningShown(false);
+    setState((prevState) => ({
+      ...prevState,
+      isSpoilerShown: true,
+      isWarningShown: false,
+    }));
   }
 
   function handleCloseWarningClick() {
-    setIsWarningShown(false);
+    setState((prevState) => ({ ...prevState, isWarningShown: false }));
   }
 
   function handleLikeBtnClick() {
-    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
+    setState((prevState) => ({
+      ...prevState,
+      numberOfLikes: prevState.numberOfLikes + 1,
+    }));
   }
 
   function handleLoveBtnClick() {
-    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 3);
+    setState((prevState) => ({
+      ...prevState,
+      numberOfLikes: prevState.numberOfLikes + 3,
+    }));
   }
 
   console.log("<App> - render");
@@ -29,11 +45,11 @@ function App() {
     <>
       <h1>Gwiezdne wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
-      <h2>Liczba polubień: {numberOfLikes}</h2>
+      <h2>Liczba polubień: {state.numberOfLikes}</h2>
       <button onClick={handleLikeBtnClick}>Lubię to!</button>
       <button onClick={handleLoveBtnClick}>Kocham to!</button>
       <h2>Fabuła</h2>
-      {isWarningShown && (
+      {state.isWarningShown && (
         <p>
           Uwaga! Opis fabuły zawiera spoilery!
           <button onClick={handleCloseWarningClick}>X</button>
@@ -41,7 +57,7 @@ function App() {
       )}
       <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
 
-      {isSpoilerShown ? (
+      {state.isSpoilerShown ? (
         <p>Vader okazuje się być ojcem Luka.</p>
       ) : (
         <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button>
