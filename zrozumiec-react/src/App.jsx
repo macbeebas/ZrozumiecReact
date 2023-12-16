cd68c1355eb00139dbeebd3b5a689e42f50965b2
Michal Viwegier <beebas@wp.pl>
2023-12-17 00:36:53 +0100

    3-6b step 5 - end code of logic grouping

--- zrozumiec-react/src/App.jsx
+++ zrozumiec-react/src/App.jsx
@@ -9,23 +9,7 @@ function App() {
       <h1>Gwiezdne wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
       <LikesCounter />
-      {/* <h2>Liczba polubień: {numberOfLikes}</h2>
-      <button onClick={handleLikeButtonClick}>Lubię to!</button>
-      <button onClick={handleLoveButtonClick}>Kocham to!</button> */}
       <Plot />
-      {/* <h2>Fabuła</h2>
-      {isWarningShown && (
-        <p>
-          Uwaga! Opis fabuły zawiera spoilery!
-          <button onClick={handleCloseWarningClick}>X</button>
-        </p>
-      )}
-      <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
-      {isSpoilerShown ? (
-        <p>Vader okazuje się być ojcem Luka.</p>
-      ) : (
-        <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button>
-      )} */}
     </>
   );
 }
