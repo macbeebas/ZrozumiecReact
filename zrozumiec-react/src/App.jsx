2942e896b6aef513d24c2ca26c22a2ce3640089b
Michal Viwegier <beebas@wp.pl>
2023-12-17 00:34:30 +0100

    3-6b step 4 - refactor - two components and cleaner code

--- zrozumiec-react/src/App.jsx
+++ zrozumiec-react/src/App.jsx
@@ -1,38 +1,19 @@
-import { useState } from "react";
+import { LikesCounter } from "./components/LikesCounter";
+import { Plot } from "./components/Plot";
 
 function App() {
-  const [isSpoilerShown, setIsSpoilerShown] = useState(false);
-  const [isWarningShown, setIsWarningShown] = useState(true);
-  const [numberOfLikes, setNumberOfLikes] = useState(50);
-
-  function handleShowSpoilerClick() {
-    setIsSpoilerShown(true);
-    console.log("handleClick!");
-    setIsWarningShown(false);
-  }
-
-  function handleCloseWarningClick() {
-    setIsWarningShown(false);
-  }
-
-  function handleLikeButtonClick() {
-    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
-  }
-
-  function handleLoveButtonClick() {
-    setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 3);
-  }
-
   console.log("<App> - render");
 
   return (
     <>
       <h1>Gwiezdne wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
-      <h2>Liczba polubień: {numberOfLikes}</h2>
+      <LikesCounter />
+      {/* <h2>Liczba polubień: {numberOfLikes}</h2>
       <button onClick={handleLikeButtonClick}>Lubię to!</button>
-      <button onClick={handleLoveButtonClick}>Kocham to!</button>
-      <h2>Fabuła</h2>
+      <button onClick={handleLoveButtonClick}>Kocham to!</button> */}
+      <Plot />
+      {/* <h2>Fabuła</h2>
       {isWarningShown && (
         <p>
           Uwaga! Opis fabuły zawiera spoilery!
@@ -44,7 +25,7 @@ function App() {
         <p>Vader okazuje się być ojcem Luka.</p>
       ) : (
         <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button>
-      )}
+      )} */}
     </>
   );
 }
