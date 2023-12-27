2aaaa5b941030a66ea7ea6f6a2ca8c2f4adc47fc
Michal Viwegier <beebas@wp.pl>
2023-12-28 00:03:59 +0100

    3-10a step 1 - sending state between Form and Review components

--- zrozumiec-react/src/App.jsx
+++ zrozumiec-react/src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { Form } from "./components/Form";
 import { LikesCounter } from "./components/LikesCounter";
 import { Plot } from "./components/Plot";
+import { Reviews } from "./components/Reviews";
+
+const initialReviews = [
+  { author: "Brian", text: "Najlepszy film ever!", id: 1 },
+  { author: "Jessica", text: "Nie podobał mi się", id: 2 },
+];
 
 function App() {
   console.log("<App> - render");
+  const [reviews, setReviews] = useState(initialReviews);
 
   return (
     <>
@@ -11,7 +19,17 @@ function App() {
       <h2>Rok produkcji: 1980</h2>
       <LikesCounter />
       <Plot />
-      <Form />
+      <Reviews reviews={reviews} />
+      <Form
+        onReviewSubmit={(author, text) => {
+          setReviews((prevReviews) => {
+            return [
+              { author, text, id: prevReviews.length + 1 },
+              ...prevReviews,
+            ];
+          });
+        }}
+      />
     </>
   );
 }
