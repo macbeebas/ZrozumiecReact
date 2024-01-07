d06f07f523501b6096d8018748e626cc1afabe60
Michal Viwegier <beebas@wp.pl>
2024-01-07 19:35:35 +0100

    Step 3 - adding border to button

--- zrozumiec-react/src/components/Item.jsx
+++ zrozumiec-react/src/components/Item.jsx
@@ -7,6 +7,15 @@ export function Item({ emoji }) {
     <li className="item">
       <span className={`emoji ${zoomed ? "zoomed" : ""}`}>{emoji}</span>
       <button
+        style={{
+          background: "blue",
+          border: "5px solid yellow",
+          "border-radius": "10px",
+          //   bo "border-radius" nie jest jednym słowem dlatego trzeba ująć w cudzysłów
+          borderRadius: "10px",
+          // albo można zapisać camelCase'm - nie może być "-" w nazwie bo React nie załapie
+          // Jednak takie stylowanie przydaje się wyłącznie do szybkiego debugowania - nie robi się tak w praktyce
+        }}
         className="btn"
         onClick={() => {
           setZoomed((wasZoomed) => !wasZoomed);
