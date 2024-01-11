f21ea56d9e00120237cba9fbb451d19e31f76617
Michal Viwegier <beebas@wp.pl>
2024-01-11 16:58:04 +0100

    Step 4 - created & finished StyledList

--- zrozumiec-react/src/components/List/List.jsx
+++ zrozumiec-react/src/components/List/List.jsx
@@ -1,5 +1,4 @@
 import { Item } from "../Item/Item";
-import styles from "./List.module.css";
 import styled from "styled-components";
 
 const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
@@ -21,23 +20,25 @@ const StyledButton = styled.button`
   }
 `;
 
+const StyledList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  gap: 30px;
+  padding: 30px;
+`;
+
 export function List() {
   return (
     <>
       <StyledButton onClick={() => alert(`Liczba emoji: ${emojis.length}`)}>
         Pokaż liczbę emoji
       </StyledButton>
-      {/* <button
-        className={styles.btn}
-        onClick={() => alert(`Liczba emoji: ${emojis.length}`)}
-      >
-        Pokaż liczbę emoji
-      </button> */}
-      <ul className={styles.list}>
+      <StyledList>
         {emojis.map((emoji) => (
           <Item key={emoji} emoji={emoji} />
         ))}
-      </ul>
+      </StyledList>
     </>
   );
 }
