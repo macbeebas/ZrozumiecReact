ddbbffbd94f17208d08d4a67b6cdf4871ea0daf7
Michal Viwegier <beebas@wp.pl>
2024-01-15 23:31:57 +0100

    Step 6 - finished styling Item and finish this module

--- zrozumiec-react/src/components/Item/Item.jsx
+++ zrozumiec-react/src/components/Item/Item.jsx
@@ -1,13 +1,5 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { StyledButton } from "../styles/StyledButton";
-
-const StyledItem = styled.li`
-  background: salmon;
-  padding: 30px 50px;
-  border-radius: 20px;
-  text-align: center;
-`;
 
 const StyledEmoji = styled.span`
   display: block;
@@ -25,9 +17,16 @@ export function Item({ emoji }) {
   const [zoomed, setZoomed] = useState(false);
 
   return (
-    <li>
-      <span>{emoji}</span>
+    <li className="bg-salmon py-8 px-12 rounded-3xl text-center overflow-hidden">
+      <span
+        className={`block mb-10 text-3xl transition-transform ${
+          zoomed ? "scale-[2]" : ""
+        }`}
+      >
+        {emoji}
+      </span>
       <button
+        className={`bg-transparent border border-solid border-current p-3 rounded-[5px] cursor-pointer w-[120px] text-white transition-colors hover:text-salmon hover:bg-white`}
         onClick={() => {
           setZoomed((wasZoomed) => !wasZoomed);
         }}
