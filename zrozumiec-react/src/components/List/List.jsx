e9747ef9459956c2ae5cd109cb821c8bc5afb259
Michal Viwegier <beebas@wp.pl>
2024-01-15 23:13:37 +0100

    Step 5 - finished styling in List & Button

--- zrozumiec-react/src/components/List/List.jsx
+++ zrozumiec-react/src/components/List/List.jsx
@@ -1,17 +1,16 @@
 import { Item } from "../Item/Item";
-import styled from "styled-components";
-import { StyledButton } from "../styles/StyledButton";
 
 const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
 
-const StyledListItem = styled(Item)`
-  margin-bottom: 50px;
-`;
+const WIDTH = 120;
 
 export function List() {
   return (
     <>
-      <button onClick={() => alert(`Liczba emoji: ${emojis.length}`)}>
+      <button
+        className={`bg-transparent border border-solid border-current p-3 rounded-[5px] cursor-pointer w-[${WIDTH}px] text-salmon transition-colors m-8 hover:text-white hover:bg-salmon`}
+        onClick={() => alert(`Liczba emoji: ${emojis.length}`)}
+      >
         Pokaż liczbę emoji
       </button>
       <ul className="flex flex-col gap-8 p-8">
