d3c126f05a527647e160b0c795b3e7e994980327
Michal Viwegier <beebas@wp.pl>
2024-02-20 23:11:22 +0100

    Module-5 - step 1 - added 'fetch'

--- module-5/front-end/src/components/Panel/Panel.jsx
+++ module-5/front-end/src/components/Panel/Panel.jsx
@@ -4,33 +4,22 @@ import { List } from "../List/List";
 import styles from "./Panel.module.css";
 
 export function Panel() {
-    const [data, setData] = useState([
-        {
-            id: 1,
-            word: "kot",
-            translation: "cat",
-            category: "noun",
-        },
-        {
-            id: 2,
-            word: "pies",
-            translation: "dog",
-            category: "noun",
-        },
-        {
-            id: 3,
-            word: "skakać",
-            translation: "jump",
-            category: "verb",
-        },
-    ]);
+  const [data, setData] = useState([]);
 
-    return (
-        <>
-            <Button>Załaduje dane</Button>
-            <section className={styles.section}>
-                <List data={data}></List>
-            </section>
-        </>
-    );
+  function handleLoadClick() {
+    fetch("http://localhost:3000/words")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+      });
+  }
+
+  return (
+    <>
+      <Button onClick={handleLoadClick}>Załaduje dane</Button>
+      <section className={styles.section}>
+        <List data={data}></List>
+      </section>
+    </>
+  );
 }
