796701f2add4e21ed64fe75c2d1b343ca14c9f89
Michal Viwegier <beebas@wp.pl>
2024-02-20 23:31:07 +0100

    5-02a - added 'useEffect' for one rerender

--- module-5/front-end/src/components/Panel/Panel.jsx
+++ module-5/front-end/src/components/Panel/Panel.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../Button/Button";
 import { List } from "../List/List";
 import styles from "./Panel.module.css";
 
 export function Panel() {
   const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/words")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+      });
+  }, []);
 
   function handleLoadClick() {
     fetch("http://localhost:3000/words")
